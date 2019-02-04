027b47331ddc362947b6466a85d2808cbb806a91
Steve Condylios <steve.condylios@reece.com.au>
2019-02-04 17:01:14 +1100

    NSC

--- chord-diagrams-and-venn-diagrams.R
+++ chord-diagrams-and-venn-diagrams.R
@@ -308,29 +308,29 @@ ggplot() +
 
 
 library(viridis) # Required for scale_fill_viridis # Very nice colour palettes: Read: https://cran.r-project.org/web/packages/viridis/vignettes/intro-to-viridis.html
-p <- ggplot() +
-  geom_polygon(data = spdf_fortified, aes(fill = nb_equip, x = long, y = lat, group = group) , size=0, alpha=0.9) +
-  theme_void() +
-  scale_fill_viridis(trans = "log", breaks=c(1,5,10,20,50,100), name="Number of restaurant", guide = guide_legend( keyheight = unit(3, units = "mm"), keywidth=unit(12, units = "mm"), label.position = "bottom", title.position = 'top', nrow=1) ) +
-  labs(
-    title = "South of France Restaurant concentration",
-    subtitle = "Number of restaurants per city district", 
-    caption = "Data: INSEE | Creation: Yan Holtz | r-graph-gallery.com"
-  ) +
-  theme(
-    text = element_text(color = "#22211d"), 
-    plot.background = element_rect(fill = "#f5f5f2", color = NA), 
-    panel.background = element_rect(fill = "#f5f5f2", color = NA), 
-    legend.background = element_rect(fill = "#f5f5f2", color = NA),
-    
-    plot.title = element_text(size= 22, hjust=0.01, color = "#4e4d47", margin = margin(b = -0.1, t = 0.4, l = 2, unit = "cm")),
-    plot.subtitle = element_text(size= 17, hjust=0.01, color = "#4e4d47", margin = margin(b = -0.1, t = 0.43, l = 2, unit = "cm")),
-    plot.caption = element_text( size=12, color = "#4e4d47", margin = margin(b = 0.3, r=-99, unit = "cm") ),
-    
-    legend.position = c(0.7, 0.09)
-  ) +
-  coord_map()
-p
+ggplot() +
+geom_polygon(data = spdf_fortified, aes(fill = nb_equip, x = long, y = lat, group = group) , size=0, alpha=0.9) +
+theme_void() +
+scale_fill_viridis(trans = "log", breaks=c(1,5,10,20,50,100), name="Number of restaurant", guide = guide_legend( keyheight = unit(3, units = "mm"), keywidth=unit(12, units = "mm"), label.position = "bottom", title.position = 'top', nrow=1) ) +
+labs(
+  title = "South of France Restaurant concentration",
+  subtitle = "Number of restaurants per city district", 
+  caption = "Data: INSEE | Creation: Yan Holtz | r-graph-gallery.com"
+) +
+theme(
+  text = element_text(color = "#22211d"), 
+  plot.background = element_rect(fill = "#f5f5f2", color = NA), 
+  panel.background = element_rect(fill = "#f5f5f2", color = NA), 
+  legend.background = element_rect(fill = "#f5f5f2", color = NA),
+  
+  plot.title = element_text(size= 22, hjust=0.01, color = "#4e4d47", margin = margin(b = -0.1, t = 0.4, l = 2, unit = "cm")),
+  plot.subtitle = element_text(size= 17, hjust=0.01, color = "#4e4d47", margin = margin(b = -0.1, t = 0.43, l = 2, unit = "cm")),
+  plot.caption = element_text( size=12, color = "#4e4d47", margin = margin(b = 0.3, r=-99, unit = "cm") ),
+  
+  legend.position = c(0.7, 0.09)
+) +
+coord_map()
+
 
 
 
